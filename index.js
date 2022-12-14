0e5090f10e5d1adbda3031b0c859a1898496782b
Oleksandr Melnychenko <melnalex@users.noreply.github.com>
2022-12-14 20:17:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
+const ping = require('ping');
 const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+    const host = req.query.host;
+    const isAlive = await checkHost(host);
+    res.send({
+        status: "ok",
+        host,
+        isAlive,
+    });
 })
 app.listen(process.env.PORT || 3000)
+
+
+const checkHost = async (host) => {
+    return new Promise((resolve, reject) => {
+        try {
+            ping.sys.probe(host, isAlive => {
+                console.log(`Ping - host: ${host}, isAlive: ${isAlive}`);
+                resolve(isAlive);
+            }, 
+            {
+                timeout: 3
+            });
+        }
+        catch(e) {
+            console.log(e.message);
+            reject(e);
+        }
+    })
+}
